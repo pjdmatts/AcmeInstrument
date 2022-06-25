0a959d50e70b1a3d8d43b4546b9f66e3477fe0d8
Peter Matthews <pjdmatthews7@gmail.com>
2022-06-25 19:16:53 -0400

    Acme

---
+++ src/acme_instrument.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory>
+#include "instrument.h"
+#include "data_in.h"
+#include "data.h"
+#include "analysis.h"
+#include "plotter.h"
+
+
+int main() {
+    
+    Instrument instrument = Instrument();
+    instrument.InstrumentGreeting();
+    //Ask for user input
+    instrument.DataInputGreeting();
+    //Create a data object from the user input
+    std::unique_ptr<Data> data(new Data());
+    instrument.DataInput();
+    instrument.DataGreeting();
+    //Ask the user what kind of analysis they want to do
+    instrument.AnalysisGreeting();
+    //Do the Analysis
+    //Make the Plot
+    instrument.PlotGreeting();
+}
